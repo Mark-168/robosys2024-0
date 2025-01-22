19d5704a89a5bdb247a2ee7bee83f567acd67fc3
Mark-168 <isseitakei11@gmail.com>
2025-01-22 16:00:34 +0900

    Add a command

--- ros2_ws/src/mypkg/mypkg/talker.py
+++ ros2_ws/src/mypkg/mypkg/talker.py
@@ -2,18 +2,6 @@ import rclpy
 from rclpy.node import Node
 from person_msgs.srv import Query
 
-class talker(Node):
-    def __init__(self):
-        super().__init__("talker")
-        self.pub = self.create_publisher(Int16, "countup", 10)
-        self.create_timer(0.5, self.cb)
-        self.n = 0
-
-
-
-
-
-
 rclpy.init()
 node = Node("talker")
 
